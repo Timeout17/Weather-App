17322d96de680927fdf1973d2357e8465a77204a
Csaba Attila <h587764@stud.u-szeged.hu>
2026-08-16 14:17:43 +0200

    A make the streamlit interface

---
+++ project/frontend/app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+
+TEXT_BACKEND_URL = "http://localhost:8000/weather"
+
+st.title("Weather bot")
+
+if city := st.chat_input("Say a random city name: "):
+
+    with st.chat_message("user"):
+        st.markdown(city)
+
+    try:
+        response = requests.post(
+            TEXT_BACKEND_URL,
+            params={"city": city}
+        )
+
+        if response.status_code == 200:
+
+            ai_answer = response.json()
+
+            with st.chat_message("assistant"):
+                st.markdown(ai_answer)
+        else:
+            st.error(
+                f"backend error! Status code: {response.status_code}"
+            )
+    except Exception as e:
+        st.error(
+            f"Not allowed the backend server: {str(e)}"
+        )

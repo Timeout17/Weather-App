cce4808faa948a77184d3097bc41547033bd81ba
Csaba Attila <h587764@stud.u-szeged.hu>
2026-08-21 19:02:35 +0200

    I add the email, the multiply city method, and now this can send email

---
+++ project/logic/orchestrator/make_answer.py
@@ -0,0 +1,48 @@
+from project.logic.services.daily_weather import DailyWeatherClass
+from project.logic.services.Messagecreate import MessageCreateClass
+from project.agent.CreateAgent import CreateClient
+from project.agent.Createmessage import CreateMessageClass
+from project.agent.LLMService import LLMServiceClass
+from project.mail.GmailService import GmailService
+import markdown
+
+
+class MakeAnswer():
+
+    async def make_answers(self):
+
+        daily_weather = DailyWeatherClass()
+        message_create = MessageCreateClass()
+
+        weather_data = await daily_weather.getWeather()
+
+        prompt = message_create.create_messages_model(weather_data)
+
+        final_message = "\n\n\n".join(prompt)
+
+        client = CreateClient().make_client()
+
+        message = CreateMessageClass().create_daily_weather_message(final_message)
+
+        ai_message = LLMServiceClass().Chatservice(client, message)
+
+        html_message = markdown.markdown(
+            ai_message.choices[0].message.content,
+            extensions=["tables"]
+        )
+
+        GmailService.send_email(
+            subject="🌤️ Mai időjárás-jelentés",
+            content=html_message
+        )
+
+        return ai_message.choices[0].message.content
+        
+import asyncio
+
+if __name__ == "__main__":
+    x = MakeAnswer()
+
+    result = asyncio.run(x.make_answers())
+
+    print(result)
